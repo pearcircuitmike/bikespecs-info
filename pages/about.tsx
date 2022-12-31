7755ea17f39a04896d82c9bdcb73cdae64521fa4
Michael Young <msyoung2012@gmail.com>
2022-12-31 12:18:23 -0500

    creating test about page
    using ts

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+const About = (): JSX.Element => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">About</h1>
+      <p>ok</p>
+    </div>
+  );
+};
+
+export default About;
