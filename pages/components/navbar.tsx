ede6e3501c7075f92264b7cc38914d27beef7f4b
Michael Young <msyoung2012@gmail.com>
2022-12-31 14:10:03 -0500

    Navbar test

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -1,12 +1,17 @@
-const Navbar = (): JSX.Element => {
+import Link from "next/link";
+
+const Navbar = () => {
   return (
     <nav>
       <div className="logo">
         <h1 className="text-3xl font-bold underline">Bike list</h1>
       </div>
-      <a>Home</a>
-      <a>About</a>
-      <a>Bikes</a>
+
+      <Link href="/">Home</Link>
+
+      <Link href="/about">About</Link>
+
+      <Link href="/bikes">Bikes</Link>
     </nav>
   );
 };
