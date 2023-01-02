85a6c880bc597d413c212247d4857a1afc0984e6
Michael Young <msyoung2012@gmail.com>
2023-01-02 01:02:07 -0500

    fix imports?

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,5 +1,5 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from "./Navbar.tsx";
+import Footer from "./Footer.tsx";
 
 const Layout = ({ children }: any): JSX.Element => {
   return (
