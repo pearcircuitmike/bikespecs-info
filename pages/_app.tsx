756942fe29f095a322514cdf08fde092d7140ba0
Michael Young <msyoung2012@gmail.com>
2023-01-15 11:07:03 -0500

    fix analytics?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
   `}
       </Script>
       <Script
